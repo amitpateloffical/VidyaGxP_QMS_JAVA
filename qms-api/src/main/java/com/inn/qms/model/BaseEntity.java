2f8fd1dd49ec53e027c51ecca8ca93a6ff0b7504
SandeepMewada21 <sandeep.mewada@vidyagxp.com>
2024-03-23 11:25:30 +0530

    Revert "Changes in BaseEntity Model class"
    This reverts commit 10bd2395ee1e6015956eb65db732edd21f57191e.

--- qms-api/src/main/java/com/inn/qms/model/BaseEntity.java
+++ qms-api/src/main/java/com/inn/qms/model/BaseEntity.java
@@ -28,11 +28,11 @@ public class BaseEntity implements Serializable {
 
     @CreatedBy
     @Column(name = "created_by", updatable = false)
-    private String createdBy;
+    private Long createdBy;
 
     @LastModifiedBy
     @Column(name = "updated_by")
-    private String updatedBy;
+    private Long updatedBy;
 
     // Getters and setters
 }
