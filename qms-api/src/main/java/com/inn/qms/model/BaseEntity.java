ad335c37a58de23ce4ec67dd8222cdcd59e0f8a0
sandeepewada21 <sandeep.mewada@vidyagxp.com>
2024-03-23 18:02:55 +0530

    change in base entity

--- qms-api/src/main/java/com/inn/qms/model/BaseEntity.java
+++ qms-api/src/main/java/com/inn/qms/model/BaseEntity.java
@@ -31,11 +31,11 @@ public class BaseEntity implements Serializable {
 
     @CreatedBy
     @Column(name = "created_by", updatable = false)
-    private String createdBy;
+    private Long createdBy;
 
     @LastModifiedBy
     @Column(name = "updated_by")
-    private String updatedBy;
+    private Long updatedBy;
 
     // Getters and setters
 }
